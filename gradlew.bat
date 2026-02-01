1cec5c89edde560856e9ff4d40ebdb4afeda3228
GravitationKing <155043267+SOnnyzeng51@users.noreply.github.com>
2026-02-01 10:33:04 +0800

    Add files via upload

---
+++ gradlew.bat
@@ -0,0 +1,34 @@
+@rem Gradle startup script for Windows
+
+@if "%DEBUG%"=="" @echo off
+@rem ##########################################################################
+@rem  Gradle startup script for Windows
+@rem ##########################################################################
+
+@rem Set local scope for the variables with windows NT shell
+if "%OS%"=="Windows_NT" setlocal
+
+set DIRNAME=%~dp0
+if "%DIRNAME%"=="" set DIRNAME=.
+
+@rem Resolve any "." and ".." in APP_HOME to make it shorter.
+for %%i in ("%DIRNAME%") do set APP_HOME=%%~fi
+
+set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+
+@rem Execute Gradle
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+
+:end
+@rem End local scope for the variables with windows NT shell
+if "%ERRORLEVEL%"=="0" goto mainEnd
+
+:fail
+rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
+rem having the window to close.
+exit /b 1
+
+:mainEnd
+endlocal
+
+:omega
